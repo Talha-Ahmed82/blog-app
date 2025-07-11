0a3271864a6b6cba74c5238fb5d21586db0bf9f0
Talha-Ahmed82 <ta8170407@gmail.com>
2025-07-11 20:46:04 +0500

    added

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "node_modules/flowbite-react/lib/esm/**/*.js",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+};
